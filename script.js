7a8dfe811fdd03de1ecaafbc969ab11eee802389
sushant-agi <sushant2508.tech@gmail.com>
2026-09-09 08:48:45 +0530

    feat: add starry background

--- script.js
+++ script.js
@@ -1,6 +1,69 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+let mouseX=-1000;
+let mouseY=-1000;
+
+canvas.addEventListener("mousemove", (event)=>{
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+});
+
+class Star {
+
+  constructor() {
+      this.x = Math.random() * canvas.width;
+      this.y = Math.random() * canvas.height;
+
+      this.radius = Math.random() * 1 + 0.3;
+
+      this.baseOpacity =
+          Math.random() * 0.6 + 0.3;
+
+      this.opacity = this.baseOpacity;
+
+      this.twinkle = Math.random() * Math.PI * 2;
+  }
+
+  update() {
+
+      this.twinkle+= 0.02;
+      let distance= Math.hypot(
+        mouseX-this.x, mouseY-this.y);
+      if (distance<70){
+        this.opacity=
+        0.5 +
+        Math.sin(this.twinkle*6) * 0.4;
+      }
+      else{
+        this.opacity=
+        this.baseOpacity +
+        Math.sin(this.twinkle) * 0.5;
+      }
+      
+  }
+
+  draw() {
+
+      ctx.beginPath();
+
+      ctx.arc(
+          this.x,
+          this.y,
+          this.radius,
+          0,
+          Math.PI * 2
+      );
+
+      ctx.fillStyle =
+          `rgba(255,255,255,${this.opacity})`;
+
+      ctx.fill();
+  }
+}
+
+
+
 const altitudeText = document.getElementById("altitudeText");
 const progressText = document.getElementById("progressText");
 const restartBtn = document.getElementById("restartBtn");
@@ -45,16 +108,28 @@ function drawPlayerAndHammer() {
   ctx.arc(playerX, playerY + 4, playerRadius, 0, Math.PI * 2);
   ctx.fill();
 }
+const starsArray = [];
+
+for (let i = 0; i < 3000; i++) {
+  starsArray.push(new Star());
+}
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  starsArray.forEach(star=> {
+    star.draw();
+  });
   
   drawPlayerAndHammer();
 }
 
 function gameLoop() {
   updatePhysics();
+  starsArray.forEach(star=> {
+    star.update();
+  });
   draw();
+  
   requestAnimationFrame(gameLoop);
 }
 
